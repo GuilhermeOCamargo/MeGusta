51bcc0b812ba3b79e12bd6b519ccfe30f0465466
Guilherme Camargo <guilherme.ocamargo@gmail.com>
2019-05-30 23:21:57 -0300

    Fixing docker environment for Eureka and Zuul

--- environment_up.sh
+++ environment_up.sh
@@ -5,8 +5,10 @@ sudo docker-compose down
 mkdir -p docker_environment/discovery/
 mkdir -p docker_environment/proxy/
 
-find . -name "pom.xml" -exec mvn package -f '{}' \;
-cp zuul-server/target/zuul-service.jar docker_environment/proxy/
+mvn clean package -f eureka-server/pom.xml
 cp eureka-server/target/eureka-service.jar docker_environment/discovery/
+
+mvn clean package -f zuul-server/pom.xml
+cp zuul-server/target/zuul-service.jar docker_environment/proxy/
 
 sudo docker-compose up -d
--- zuul-server/src/main/java/br/com/megusta/zuulserver/ZuulServerApplication.java
+++ zuul-server/src/main/java/br/com/megusta/zuulserver/ZuulServerApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
+@PropertySource("classpath:application.properties")
 public class ZuulServerApplication {
 
 	public static void main(String[] args) {
